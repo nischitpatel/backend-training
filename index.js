3bafc754df421467bfe0bbf47bc0566fc76790fb
nischitpatel <np58845@gmail.com>
2026-01-15 10:33:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Serve is listening at http://localhost:${PORT}...`);
+})
